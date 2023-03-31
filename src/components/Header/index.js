761718f9dcf531ba910bb7054fc1388b06afb0b6
saimanikanta17 <sivasatya1798@gmail.com>
2023-03-31 07:41:52 +0000

    covid19Dashboard

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,39 +4,23 @@ import './index.css'
 
 const Header = () => (
   <nav className="nav-bar">
-    <div>
+    <Link to="/" className="link-style">
+      <h1 className="logo">
+        COVID19<span className="span-logo">INDIA</span>
+      </h1>
+    </Link>
+    <ul className="nav-links-container">
       <Link to="/" className="link-style">
-        <h1 className="logo">
-          COVID19<span className="span-logo">INDIA</span>
-        </h1>
+        <li>
+          <button type="button">Home</button>
+        </li>
       </Link>
-      <ul className="nav-links-container">
-        <Link to="/" className="link-style">
-          <li>
-            <button type="button">Home</button>
-          </li>
-        </Link>
-        <Link to="/about" className="link-style">
-          <li>
-            <button type="button">About</button>
-          </li>
-        </Link>
-      </ul>
-    </div>
-    <div>
-      <ul className="nav-links-sm-container">
-        <Link to="/" className="link-style">
-          <li>
-            <button type="button">Home</button>
-          </li>
-        </Link>
-        <Link to="/about" className="link-style">
-          <li>
-            <button type="button">About</button>
-          </li>
-        </Link>
-      </ul>
-    </div>
+      <Link to="/about" className="link-style">
+        <li>
+          <button type="button">About</button>
+        </li>
+      </Link>
+    </ul>
   </nav>
 )
 
